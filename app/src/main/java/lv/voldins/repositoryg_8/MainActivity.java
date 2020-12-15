66ddc24cd68bef441cbb8f46d8bfb16ca246e0f2
voudis777 <55672178+voudis777@users.noreply.github.com>
2020-12-15 23:47:18 +0200

    PD3

---
+++ app/src/main/java/lv/voldins/repositoryg_8/MainActivity.java
@@ -0,0 +1,84 @@
+package lv.voldins.repositoryg_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private Button activity2;
+    private Button save;
+    EditText text;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        save = (Button) findViewById(R.id.save);
+        preferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePreferences();
+            }
+        });
+
+        activity2 = (Button) findViewById(R.id.activity2);
+        activity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.themes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(MainActivity.this);
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+    public void savePreferences(){
+        text.findViewById(R.id.writedText);
+        editor = preferences.edit();
+        editor.putString("", text.getText().toString());
+        editor.commit();
+        Toast.makeText(MainActivity.this,"Preference saglabāta", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Atzīmēta tēma: " +text,Toast.LENGTH_SHORT).show();
+        if (position == 0){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
+        if(position == 1){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        if(position == 2){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
---
+++ app/src/main/java/lv/voldins/repositoryg_8/MainActivity2.java
@@ -0,0 +1,41 @@
+package lv.voldins.repositoryg_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button activity1;
+    private Button readPreferences;
+    TextView text;
+    SharedPreferences preferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        activity1 = (Button) findViewById(R.id.activity1);
+        activity1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+        readPreferences = (Button) findViewById(R.id.preferences);
+        readPreferences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+                String s1 = preferences.getString("name", "");
+                text = (TextView) findViewById(R.id.textView);
+                text.setText(s1);
+            }
+        });
+    }
+    public void openActivity1(){
+        finish();
+    }
+}
